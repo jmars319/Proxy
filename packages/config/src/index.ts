fe489d098516f5ce6f89a27518a8039d027bea99
Jason Marshall <97139706+jmars319@users.noreply.github.com>
2026-05-29 11:53:52 -0400

    fix: allow local suite send origins

--- packages/config/src/index.ts
+++ packages/config/src/index.ts
@@ -65,9 +65,13 @@ const defaultSuiteAllowedOrigins = [
   "http://localhost:3000",
   "http://localhost:3001",
   "http://localhost:4173",
+  "http://localhost:5173",
+  "http://localhost:5176",
   "http://127.0.0.1:3000",
   "http://127.0.0.1:3001",
-  "http://127.0.0.1:4173"
+  "http://127.0.0.1:4173",
+  "http://127.0.0.1:5173",
+  "http://127.0.0.1:5176"
 ];
 
 export const readSuiteEndpointConfig = (
